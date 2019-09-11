88edc9443c908b2cc1624af02f3ba19872798806
Javier Zapata <zapataj87@gmail.com>
2019-09-10 22:07:14 -0500

    added more services

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
-
+import Layout from '@common/Layout';
+import { Heading, Box, Flex, Card } from 'rebass';
 
 const Services = (props) => {
   const services = props.data.allMarkdownRemark.edges;
   return (
     <>
 
-      <div className="intro">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <h1>Services</h1>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="container pb-6">
-        <div className="row">
+        <Box
+          sx={{
+          px: 4,
+          py: 6,
+          backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://source.unsplash.com/random/1024x768?sky)',
+          backgroundSize: 'cover',
+          color: 'white',
+          bg: 'gray',
+          }}>
+            <Heading
+            textAlign='center'
+            fontSize={[ 5, 6 ]}>
+            Services
+            </Heading>
+      </Box>
+      <Flex>
           {services.map(edge => (
-            <div key={edge.node.frontmatter.path} className="col-12 col-md-4 mb-1">
-              <div className="card service service-teaser">
-                <div className="card-content">
-                  <h2>
-                    <Link to={edge.node.frontmatter.path}>{edge.node.frontmatter.title}</Link>
-                  </h2>
-                  <p>{edge.node.excerpt}</p>
-                </div>
-              </div>
-            </div>
+            <Card width={[1,1/2,1/4]} m={4} sx={{borderStyle: "solid"}}>
+              <h2>
+                <Link to={edge.node.frontmatter.path}>{edge.node.frontmatter.title}</Link>
+              </h2>
+              <p>{edge.node.excerpt}</p>
+            </Card>
           ))}
-        </div>
-      </div>
+      </Flex>
+
     </>
   );
 };
